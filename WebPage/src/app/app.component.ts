6651897a9820cea81ed819c329e9ff656d8e9815
U.H. Chathurya Hewage <111672583+hewageuhcu@users.noreply.github.com>
2024-09-09 02:59:03 +0900

    🥳

--- WebPage/src/app/app.component.ts
+++ WebPage/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NewComponentComponent} from '../new-component/new-component.component' 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NewComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
