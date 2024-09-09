a6a61871a31b15ed99b93526f3e711ba8a5c4748
U.H. Chathurya Hewage <111672583+hewageuhcu@users.noreply.github.com>
2024-09-10 00:14:50 +0900

    🙃

--- WebPage/src/app/app.module.ts
+++ WebPage/src/app/app.module.ts
@@ -1,19 +1,19 @@
-// import { NgModule } from '@angular/core';
-// import { BrowserModule } from '@angular/platform-browser';
-// import { FormsModule } from '@angular/forms';
-// import { AppComponent } from './app.component';
-// import {NewComponentComponent} from '../new-component/new-component.component' 
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import {NewComponentComponent} from '../new-component/new-component.component' 
 
-// @NgModule({
-//   declarations: [
-//     AppComponent,
-//     NewComponentComponent 
-//   ],
-//   imports: [
-//     BrowserModule,
-//     FormsModule 
-//   ],
-//   providers: [],
-//   bootstrap: [AppComponent]
-// })
-// export class AppModule { }
+@NgModule({
+  declarations: [
+    AppComponent
+    
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule 
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
