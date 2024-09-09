6c1db2517c0e6845d6b1c02e00c415aa3dc4e142
U.H. Chathurya Hewage <111672583+hewageuhcu@users.noreply.github.com>
2024-09-09 20:59:32 +0900

    data binding

--- WebPage/src/app/app.component.spec.ts
+++ WebPage/src/app/app.component.spec.ts
@@ -17,7 +17,7 @@ describe('AppComponent', () => {
   it(`should have the 'WebPage' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('WebPage');
+    expect(app.firstName).toEqual('WebPage');
   });
 
   it('should render title', () => {
