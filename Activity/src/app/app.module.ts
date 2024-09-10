e6f4dc5eb138a172b62de632930068502b079fe3
U.H. Chathurya Hewage <111672583+hewageuhcu@users.noreply.github.com>
2024-09-10 20:51:11 +0900

    created app.module.ts

---
+++ Activity/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+   
+   
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule // Include FormsModule for two-way binding
+  ],
+  providers: [],
+ 
+})
+export class AppModule { }
