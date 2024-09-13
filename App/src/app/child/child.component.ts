5362eb6bed2d9fdf0a143747a14e21f5a7b81e83
U.H. Chathurya Hewage <111672583+hewageuhcu@users.noreply.github.com>
2024-09-14 03:58:55 +0900

    😒

--- App/src/app/child/child.component.ts
+++ App/src/app/child/child.component.ts
@@ -12,4 +12,7 @@ ngOnInit(): void {
   throw new Error('Method not implemented.');
 }
 channelName='Chathurya';
+
+constructor(){}
+
 }
--- App/src/app/parent/parent.component.ts
+++ App/src/app/parent/parent.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ChildComponent } from "../child/child.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-parent',
   standalone: true,
-  imports: [ChildComponent],
+  imports: [ChildComponent,FormsModule],
   templateUrl: './parent.component.html',
   styleUrl: './parent.component.css'
 })
