985e36e92d134a474d13c262e1b23f33d58b261c
U.H. Chathurya Hewage <111672583+hewageuhcu@users.noreply.github.com>
2024-09-14 03:17:13 +0900

    🙂

--- App/src/app/app.component.ts
+++ App/src/app/app.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ParentComponent } from "./parent/parent.component";
+import { NgModule } from '@angular/core';
+import { ChildComponent } from './child/child.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule}
 
-@Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, ParentComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+@NgModule({
+  declarations:[
+    Component,
+    ParentComponent,
+    ChildComponent
+  ],
+  imports:[
+    BrowserModule,
+    FormsModule
+  ],
+  providers:[],
+  bootstrap:[Component]
 })
-export class AppComponent {
-  title = 'App';
-}
+export class AppModule{}
