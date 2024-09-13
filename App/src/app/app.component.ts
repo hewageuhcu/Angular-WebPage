53d65776f302e3a1e4df2c07b666fac0f25a0b72
U.H. Chathurya Hewage <111672583+hewageuhcu@users.noreply.github.com>
2024-09-14 00:26:06 +0900

    🥳

--- App/src/app/app.component.ts
+++ App/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ParentComponent } from "./parent/parent.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ParentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
