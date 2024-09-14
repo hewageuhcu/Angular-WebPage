6c3ab9da23c42be9e1c41f129ae3cce4c67f1741
U.H. Chathurya Hewage <111672583+hewageuhcu@users.noreply.github.com>
2024-09-15 04:30:42 +0900

    🥳

--- angular-routing/src/app/app.component.ts
+++ angular-routing/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AboutComponent } from "./about/about.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
