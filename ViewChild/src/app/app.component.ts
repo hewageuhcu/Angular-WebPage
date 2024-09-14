f2995aef4d7cb035b89503e9affade5dacdf7593
U.H. Chathurya Hewage <111672583+hewageuhcu@users.noreply.github.com>
2024-09-15 02:44:51 +0900

    😤

--- ViewChild/src/app/app.component.ts
+++ ViewChild/src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Forms}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'ViewChild';
+export class Component implements OnInit {
+  @viewChild('myInput', {static:true}) myCustomInput;
+
+ngOnInit(){
+  this.myCustomInput.nativeElement.focus();
+}
+
+sendInput(input,channel){
+console.log(input.value);
+console.log(channel.innerText);
+}
 }
