06b36a7fcdaa83a044da94c79c2703c80518125d
U.H. Chathurya Hewage <111672583+hewageuhcu@users.noreply.github.com>
2024-09-15 02:47:36 +0900

    😤

---
+++ ViewChild/src/app/app.module.ts
@@ -0,0 +1,5 @@
+<div>
+<p #channelName> Chathurya </p>
+<input type="text" #myInput ngModel>
+<button [disabled]="myInput.value.length===0"(click)="sendInput(myInput,channelName")Click</button>
+</div>
