2c0834a25b8d67b5b2bb08fcceac70af25bdf158
U.H. Chathurya Hewage <111672583+hewageuhcu@users.noreply.github.com>
2024-09-14 17:24:34 +0900

    😤

--- DataInjection/src/app/app.component.ts
+++ DataInjection/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FirstComponent } from "./first/first.component";
+import { SecondComponent } from './second/second.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FirstComponent],
+  imports: [RouterOutlet, FirstComponent,SecondComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
