d168a9af72a0d653950dba9c9aed632ee62e39a8
U.H. Chathurya Hewage <111672583+hewageuhcu@users.noreply.github.com>
2024-09-15 16:43:09 +0900

    creaed new component

---
+++ angular-routing/src/app/all-products/all-products.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-all-products',
+  standalone: true,
+  imports: [],
+  templateUrl: './all-products.component.html',
+  styleUrl: './all-products.component.css'
+})
+export class AllProductsComponent {
+
+}
