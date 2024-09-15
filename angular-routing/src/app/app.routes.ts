cc6e530be3c0e5e9fee7189ccfe224c23267291b
U.H. Chathurya Hewage <111672583+hewageuhcu@users.noreply.github.com>
2024-09-15 15:37:44 +0900

    😤

--- angular-routing/src/app/app.routes.ts
+++ angular-routing/src/app/app.routes.ts
@@ -6,7 +6,7 @@ import { ContactComponent } from './contact/contact.component';
 import { ProductComponent } from './product/product.component';
 
 export const routes: Routes = [
-    {path:'',component:HomeComponent},
+    {path:'',redirectTo:'home',pathMatch:'full'},
     {path:'home',component:HomeComponent},
     {path:'about',component:AboutComponent},
     {path:'contact',component:ContactComponent},
