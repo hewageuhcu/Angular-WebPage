a889d85101130385f9e330879381f8d89faf1418
U.H. Chathurya Hewage <111672583+hewageuhcu@users.noreply.github.com>
2024-09-15 14:40:46 +0900

    🥳

--- angular-routing/src/app/app.routes.ts
+++ angular-routing/src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { ContactComponent } from './contact/contact.component';
 import { ProductComponent } from './product/product.component';
 
 export const routes: Routes = [
+    {path:'',component:HomeComponent},
    {path:'home',component:HomeComponent},
     {path:'about',component:AboutComponent},
     {path:'contact',component:ContactComponent},
