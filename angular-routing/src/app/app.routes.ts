19b5420d3585df993e23f0e77bd0942303bc0cf1
U.H. Chathurya Hewage <111672583+hewageuhcu@users.noreply.github.com>
2024-09-15 14:38:58 +0900

    🥳

--- angular-routing/src/app/app.routes.ts
+++ angular-routing/src/app/app.routes.ts
@@ -9,7 +9,7 @@ export const routes: Routes = [
    {path:'home',component:HomeComponent},
     {path:'about',component:AboutComponent},
     {path:'contact',component:ContactComponent},
-    {path:'products',component:ProductComponent},
+    {path:'product',component:ProductComponent},
 ];
 
 @NgModule({
