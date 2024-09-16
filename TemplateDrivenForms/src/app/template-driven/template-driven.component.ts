bc9893ceb057c5ff5a1f0b6a92e18561418b09b6
U.H. Chathurya Hewage <111672583+hewageuhcu@users.noreply.github.com>
2024-09-17 03:23:49 +0900

    component created

---
+++ TemplateDrivenForms/src/app/template-driven/template-driven.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-template-driven',
+  standalone: true,
+  imports: [],
+  templateUrl: './template-driven.component.html',
+  styleUrl: './template-driven.component.css'
+})
+export class TemplateDrivenComponent {
+
+}
