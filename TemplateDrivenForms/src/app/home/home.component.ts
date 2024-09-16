b73287da67478c7481acf7ac838cab307ed74a77
U.H. Chathurya Hewage <111672583+hewageuhcu@users.noreply.github.com>
2024-09-17 03:29:30 +0900

    😊

--- TemplateDrivenForms/src/app/home/home.component.ts
+++ TemplateDrivenForms/src/app/home/home.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  standalone: true,
-  imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  constructor(private router: Router) {}
 
+  ngOnInit(): void {}
+
+  goToTemplateDriven() {
+    this.router.navigate(['template-driven']);
+  }
+
+  goToModelDriven() {
+    this.router.navigate(['model-driven']);
+  }
 }
