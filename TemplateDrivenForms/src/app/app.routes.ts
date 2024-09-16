55110fff5a9808ec4fa4ecec6e04d71464abf0fa
U.H. Chathurya Hewage <111672583+hewageuhcu@users.noreply.github.com>
2024-09-17 03:30:31 +0900

    😊

--- TemplateDrivenForms/src/app/app.routes.ts
+++ TemplateDrivenForms/src/app/app.routes.ts
@@ -1,3 +1,34 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ReactiveComponent } from './reactive/reactive.component';
+import { TemplateDrivenComponent } from './template-driven/template-driven.component';
 
-export const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'template-driven',
+    component: TemplateDrivenComponent,
+  },
+  {
+    path: 'model-driven',
+    component: ReactiveComponent,
+  },
+  {
+    path: '**',
+    component: HomeComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
