2a0eee99aaa6fa1f3e334660757719a99e14f2f6
U.H. Chathurya Hewage <111672583+hewageuhcu@users.noreply.github.com>
2024-09-17 03:34:00 +0900

    😊

--- TemplateDrivenForms/src/app/reactive/reactive.component.ts
+++ TemplateDrivenForms/src/app/reactive/reactive.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
-  standalone: true,
-  imports: [],
   templateUrl: './reactive.component.html',
-  styleUrl: './reactive.component.css'
+  styleUrls: ['./reactive.component.css']
 })
-export class ReactiveComponent {
+export class ReactiveComponent implements OnInit {
+
+  myForm: FormGroup;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.myForm = new FormGroup(
+      {
+        uname: new FormControl('Surender', Validators.required),
+        email: new FormControl(null, [Validators.required, Validators.email]),
+        readTerms: new FormControl(false)
+      }
+    );
+  }
+
+  onSubmit() {
+    this.myForm.reset();
+  }
+
+  accept() {
+    this.myForm.setValue({
+      readTerms: true
+    });
+  }
 
 }
